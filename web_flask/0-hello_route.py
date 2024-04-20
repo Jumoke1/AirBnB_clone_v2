d12a3305c39ae745d5b33f00807f0a590f5fbeb0
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-20 13:18:30 -0700

    python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,16 @@
 Routes:
 /: display “Hello HBNB!
 """
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays hello hbnb"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' port=5000)
