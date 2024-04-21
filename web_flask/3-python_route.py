8370ae522321fdd5623c8ef758faeacaab520d92
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-21 19:49:25 +0000

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,20 +14,24 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """displays hello hbnb"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """displays hbnb"""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """displays C and the value of the text"""
     formatted_text = text.replace('_', ' ')
     return 'C {}'.format(formatted_text)
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):  # Changed 'test' to 'text', added default value
+def python_text(text='is cool'):
+    """displays python and the value of the text"""
     formatted_text = text.replace('_', ' ')
     return 'Python {}'.format(formatted_text)
 
