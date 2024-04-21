9e0d28b230ae5e943b1cda80a2661520491fa587
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-21 23:20:10 +0000

    route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from flask import Flask
-
 """ 
 Start Flask application
 Routes:
@@ -10,32 +8,36 @@ Routes:
     /python/<text>: Display 'Python', followed by the value of the text.
     /number/<int:n>: Displays '<n> is a number' only if <n> is an integer.
 """
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """displas hello hbnb"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """display hbnb"""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """displays C and the value of the text"""
     formatted_text = text.replace('_', ' ')
     return 'C {}'.format(formatted_text)
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):  # Changed 'test' to 'text', added default value
+def python_text(text='is cool'):
+    """displays python and text"""
     formatted_text = text.replace('_', ' ')
     return 'Python {}'.format(formatted_text)
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return "{:d} is a number".format(n)  # Corrected 'forman' to 'format'
+    """displays the value of the number"""
+    return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
