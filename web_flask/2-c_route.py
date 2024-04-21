c30dd51e293dbfe88430ac9270da5359d76bc0f1
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-21 23:37:12 +0000

    pyth

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" 
-start flask application
-routes:
- /: Displays 'Hello HBNB!'.
+"""
+Start Flask application
+Routes:
+    /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
 """
@@ -13,21 +13,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays hello hbnb"""
-    return "Hello HBNB!"
+    """displays hello hbnb"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """displays hbnb"""
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """displays the text"""
-    formatted_text = text.replace('_', '')
-    return "C {}".format(formatted_text)
+    """displays C and the value of the text"""
+    formatted_text = text.replace('_', ' ')
+    return 'C {}'.format(formatted_text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
