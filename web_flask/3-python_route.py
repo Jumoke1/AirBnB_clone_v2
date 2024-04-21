4ff98a6b78113319edcb3afb8f2ff66f58283178
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-21 20:03:51 +0000

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
-
-""" 
+"""
 Start Flask application
 Routes:
     /: Displays 'Hello HBNB!'.
@@ -9,24 +7,29 @@ Routes:
     /c/<text>: Displays 'C' followed by the value of <text>.
     /python/<text>: Display 'Python', followed by the value of the text.
 """
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """displays hello hbnb"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """displays hbnb"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """displays C and the value of the text"""
     formatted_text = text.replace('_', ' ')
     return 'C {}'.format(formatted_text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
